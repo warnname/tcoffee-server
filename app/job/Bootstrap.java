58a6a526af56bc0ca1e1c88a7077e515de6ac0b3
paolo.ditommaso <paolo.ditommaso@cb409b52-f269-45c4-4a06-1bd0eacd059b>
2010-12-03 15:05:28 +0000

    [tserver] removed unsed properties
    git-svn-id: https://tcoffee.googlecode.com/svn/tserver/trunk@463 cb409b52-f269-45c4-4a06-1bd0eacd059b

--- app/job/Bootstrap.java
+++ app/job/Bootstrap.java
@@ -1,19 +1,10 @@
 package job;
 
-import java.io.File;
-import java.io.IOException;
-
-import models.AppProps;
-
-import org.apache.commons.io.FileUtils;
-
-import play.Logger;
 import play.Play;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.mvc.Router;
 import play.vfs.VirtualFile;
-import exception.QuickException;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
@@ -25,8 +16,6 @@ public class Bootstrap extends Job {
 
 		addTestsRoute();
 		
-		deployPropFile();
-		
 	}
 	
 	private void addTestsRoute() {
@@ -37,31 +26,6 @@ public class Bootstrap extends Job {
 		
 	}
 
-
-	private void deployPropFile() {
-		File source = Play.getFile("conf/tserver.properties.xml");
-		File target = AppProps.SERVER_PROPS_FILE;
-		
-		if( source.equals(target) ) {
-			return;
-		}
-		
-		/*
-		 * check if properties file exist in data folder otherwise copy them 
-		 */
-		boolean force = "true".equals(Play.configuration.getProperty("deploy.props.file"));
-
-		if( !target.exists() || force ) {
-
-			Logger.info("Copy default t-server properties file to: %s", target);
-			try {
-				FileUtils.copyFile(source, target);
-			} catch (IOException e) {
-				throw new QuickException(e, "Unable to copying tserver properties file");
-			}
-		}
-	}
-	
 
 
 }
