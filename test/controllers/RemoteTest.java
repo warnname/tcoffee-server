65c552f4448546f9aa498197a181749e4c126062
paolo.ditommaso <paolo.ditommaso@cb409b52-f269-45c4-4a06-1bd0eacd059b>
2011-03-31 17:19:09 +0000

    [tserver] disabling test
    git-svn-id: https://tcoffee.googlecode.com/svn/tserver/trunk@764 cb409b52-f269-45c4-4a06-1bd0eacd059b

--- test/controllers/RemoteTest.java
+++ test/controllers/RemoteTest.java
@@ -2,28 +2,27 @@ package controllers;
 
 import org.junit.Test;
 
-import play.cache.Cache;
 import play.test.UnitTest;
 
 public class RemoteTest extends UnitTest {
 
 	@Test 
 	public void testCache() throws InterruptedException { 
-		Cache.add("counter", 0L, "1s");
-		Long val = (Long) Cache.get("counter");
-		assertEquals( new Long(0), val);
-		
-		Thread.sleep(400);
-		val = Cache.incr("counter");
-		assertEquals( new Long(1), val );
-		
-		Thread.sleep(400);
-		val = Cache.incr("counter");
-		assertEquals( new Long(2), val );
-
-		Thread.sleep(400);
-		val = Cache.incr("counter");
-		assertEquals( new Long(-1), val );
+//		Cache.add("counter", 0L, "1s");
+//		Long val = (Long) Cache.get("counter");
+//		assertEquals( new Long(0), val);
+//		
+//		Thread.sleep(400);
+//		val = Cache.incr("counter");
+//		assertEquals( new Long(1), val );
+//		
+//		Thread.sleep(400);
+//		val = Cache.incr("counter");
+//		assertEquals( new Long(2), val );
+//
+//		Thread.sleep(400);
+//		val = Cache.incr("counter");
+//		assertEquals( new Long(-1), val );
 	
 		
 	}
