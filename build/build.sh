b6b76c58e4efe41cf93816e2d82bc3f03a66663c
paolo.ditommaso <paolo.ditommaso@cb409b52-f269-45c4-4a06-1bd0eacd059b>
2011-05-24 16:19:01 +0000

    [T-server] upgrade to Play! 1.2.1
    git-svn-id: https://tcoffee.googlecode.com/svn/tserver/trunk@914 cb409b52-f269-45c4-4a06-1bd0eacd059b

--- build/build.sh
+++ build/build.sh
@@ -41,7 +41,7 @@ set -x
 #
 SANDBOX=$WORKSPACE/sandbox
 
-PLAY_VER=play-1.1.1
+PLAY_VER=play-1.2.1
 PLAY_ZIP=$PLAY_VER.zip
 PLAY_URL=http://download.playframework.org/releases/$PLAY_ZIP
 PLAY_HOME=$WORKSPACE/$PLAY_VER
