0d9aad1ef41e5f76d577058de2f7de65ef7d9817
paolo.ditommaso <paolo.ditommaso@cb409b52-f269-45c4-4a06-1bd0eacd059b>
2012-11-16 09:47:02 +0000

    [T-server] Dropped Cat-Rapid module
    git-svn-id: https://tcoffee.googlecode.com/svn/tserver/trunk@1417 cb409b52-f269-45c4-4a06-1bd0eacd059b

--- build/bundles/crg/catrapid/bin/combine.2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# for some reasons pro and rna are swapped, this is the correct way of using them
-paste ./coefficients.pro.txt ./data/bait.test | awk '{for(i=2;i<=NF;i++){printf "%f\t", $i*$1} printf "\n"}' | awk '{for(i=1;i<=NF;i++){a[i]=a[i]+$i}} END{for(i=1;i<=NF;i++){printf "%f\t", a[i]} printf "\n"}' > ./data/bait.combined.txt
-paste ./coefficients.rna.txt ./data/prey.test | awk '{for(i=2;i<=NF;i++){printf "%f\t", $i*$1} printf "\n"}' | awk '{for(i=1;i<=NF;i++){a[i]=a[i]+$i}} END{for(i=1;i<=NF;i++){printf "%f\t", a[i]} printf "\n"}' > ./data/prey.combined.txt
-
--- build/bundles/crg/catrapid/bin/run.set.sh
+++
@@ -1,9 +0,0 @@
-for j in `cat interactions.txt | grep -v "#" `; do 
-# rna is the first entry
-i1=`echo $j | sed 's/+/ /g' | awk '{print $1}'`; 
-i2=`echo $j | sed 's/+/ /g' | awk '{print $2}'`; 
-si1=`awk '($1=="'$i1'"){print $2}' run.interactions.txt` ; 
-si2=`awk '($1=="'$i2'"){print $2}' run.interactions.txt` ; 
-#protein goes first
-sh start.modified.sh $i2 $si2 $i1 $si1 ; 
-done  
