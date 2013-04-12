af71b4a4cd0b922408172a88c4c487c15af8b23b
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2013-04-12 12:50:11 +0200

    refactoring dir structure

--- build/bundles/crg/bigtree/bin/bigrunner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p $TMP_4_TCOFFEE
-mkdir -p $LOCKDIR_4_TCOFFEE
-mkdir -p $CACHE_4_TCOFFEE
-python $BIG_HOME/bigbigtree.py "$@"
-if [ ! -e $DATA_PATH/result/trees/final.ph ]; then 
-  exit 1
-fi 
--- build/bundles/crg/tcoffee/matrix/README.txt
+++
@@ -1,4 +0,0 @@
-#
-# This folder contains matrix required by m-coffee mode
-# The MCOFFEE_4_TCOFFEE env variable have to refence this path
-#
