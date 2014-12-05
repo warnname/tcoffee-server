b0c4a8528c96f898fb00644b86c9a20cbf7d6a23
smoretti <smoretti@unil.ch>
2014-12-05 10:40:55 +0100

    Change paths for new machine + add daemon stop script

--- misc/start.sh
+++ misc/start.sh
@@ -1,10 +1,7 @@
-cd /var/lib/tcoffee/
-#sudo -u tcoffee sh -c 'source /mnt/common/etc/profile.d/vitalit_lsf.sh; export PATH=/mnt/local/bin:$PATH; /var/lib/tcoffee/play/play start ./tserver --%vital'
+cd /var/vhosts/tcoffee/
 sudo -u tcoffee sh -c 'source /mnt/common/etc/profile.d/vitalit_lsf.sh;
-                       source /etc/profile.d/modules.sh;
-                       export PATH=/software/bin:$PATH;
-                       module use /software/module/;
-                       module add SequenceAnalysis/MultipleSequenceAlignment/T-Coffee/9.03.r1318;
+                       source /etc/profile.d/vitalit.sh;
+                       module add SequenceAnalysis/MultipleSequenceAlignment/T-Coffee/11.00.8cbe486;
                        module add SequenceAnalysis/ProtoGene/4.2.0;
-                       /var/lib/tcoffee/play/play start ./tserver --%vital'
+                       /var/vhosts/tcoffee/play/play start ./tserver --%vital'
 cd - >/dev/null
---
+++ misc/stop.sh
@@ -0,0 +1,3 @@
+cd /var/vhosts/tcoffee/
+sudo -u tcoffee sh -c 'source /mnt/common/etc/profile.d/vitalit_lsf.sh; /var/vhosts/tcoffee/play/play stop ./tserver'
+cd - >/dev/null
